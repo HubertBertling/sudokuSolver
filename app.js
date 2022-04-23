30a39f9e78efeaca6e74291e2357ed74d04f2c4c
HubertBertling <h.bertling@web.de>
2022-04-23 22:30:35 +0200

    Zwischenstand

--- app.js
+++ app.js
@@ -176,7 +176,7 @@ class SudokuApp {
         document.querySelector('#btn-save').addEventListener('click', () => {
             sudoApp.saveBtnPressed();
         });
-        // Der Wiederherstellen--Button: Ein gespeicherter Zustand wird wiederhergestellt.
+ /*       // Der Wiederherstellen--Button: Ein gespeicherter Zustand wird wiederhergestellt.
         document.querySelector('#btn-restore').addEventListener('click', () => {
             sudoApp.restoreBtnPressed();
         });
@@ -447,6 +447,7 @@ class SudokuApp {
                 this.sudokuTestCaseStorage.init();
             }
             this.sudokuTestCaseStorage.displayCurrentTC();
+            this.sortedSudokuPuzzleTable = new SortedSudokuPuzzleTable();
         }
     }
 
@@ -666,6 +667,7 @@ class ProgressBar {
     constructor() {
         this.elemPlay = document.getElementById("myBarPlay");
         this.elemDef = document.getElementById("myBarDef");
+        this.elemUnset = document.getElementById("total-bar-value");
     }
     init() {
         this.elemPlay.style.width = "10%"
@@ -691,6 +693,7 @@ class ProgressBar {
             this.elemPlay.innerHTML = playCount;
             this.elemPlay.style.paddingRight = "5px"
         }
+        this.elemUnset.innerHTML = 81 - totalCount;
     }
 }
 
@@ -977,7 +980,7 @@ class AutomatedRunnerOnGrid {
         this.displayDepth();
         this.displayAutoDirection();
         this.displayProgress();
-        this.displayGoneSteps();
+        //      this.displayGoneSteps();
         this.displaySpeedSetting();
     }
     displaySpeedSetting() {
@@ -985,11 +988,11 @@ class AutomatedRunnerOnGrid {
         element.value = this.execSpeedLevel;
     }
 
-    displayGoneSteps() {
-        let goneStepsNode = document.getElementById("step-count");
-        goneStepsNode.textContent = this.goneSteps;
-    }
-
+    /*    displayGoneSteps() {
+            let goneStepsNode = document.getElementById("step-count");
+            goneStepsNode.textContent = this.goneSteps;
+        }
+    */
     displayAutoDirection() {
         let forwardNode = document.getElementById("radio-forward");
         let backwardNode = document.getElementById("radio-backward");
@@ -2927,7 +2930,148 @@ class FileDialog {
 
 }
 
+class SortedSudokuPuzzleTable {
+    constructor() {
+        this.sort_Table = document.getElementById("puzzle-db-table");
+        this.tableSort(sort_Table);
+    }
+    tableSort(tab) {
+        let thead = tab.tHead;
+        let tr_in_thead, tabletitel;
+        if (thead) tr_in_thead = thead.rows;
+        if (tr_in_thead) tabletitel = tr_in_thead[0].cells;
+        if (!(tabletitel && tabletitel.length > 0)) {
+            console.error("Tabelle hat keinen Kopf und/oder keine Kopfzellen.");
+            return;
+        }
+        let tbdy = tab.tBodies;
+        if (!(tbdy)) {
+            console.error("Tabelle hat keinen tbody.");
+            return;
+        }
+        tbdy = tbdy[0];
+        const tr = tbdy.rows;
+        if (!(tr && tr.length > 0)) {
+            console.error("Tabelle hat keine Zeilen im tbody.");
+            return;
+        }
+        const nrows = tr.length,
+            ncols = tr[0].cells.length;
+        // Einige Variablen
+        let arr = [],
+            sorted = -1,
+            sortbuttons = [],
+            sorttype = [];
+        // Hinweistexte
+        const sort_info = {
+            asc: "Tabelle ist aufsteigend nach dieser Spalte sortiert",
+            desc: "Tabelle ist absteigend nach dieser Spalte sortiert",
+        };
+        const sort_hint = {
+            asc: "Sortiere aufsteigend nach ",
+            desc: "Sortiere absteigend nach ",
+        };
+        // Sortiersymbol
+        const sortsymbol =
+            '<svg role="img" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="-5 -5 190 110"><path  d="M0 0 L50 100 L100 0 Z" style="stroke:currentColor;fill:transparent;stroke-width:10;"/><path d="M80 100 L180 100 L130 0 Z" style="stroke:currentColor;fill:transparent;stroke-width:10;"/></svg>';
+        // Stylesheets für Button im TH
+        if (!document.getElementById("Stylesheet_tableSort")) {
+            const sortbuttonStyle = document.createElement('style');
+            const stylestring =
+                '.sortbutton { width: 100%; height: 100%; border: none; background-color: transparent; font: inherit; color: inherit; text-align: inherit; padding: 0; cursor: pointer; } ' +
+                '.sortierbar thead th span.visually-hidden { position: absolute !important; clip: rect(1px, 1px, 1px, 1px) !important; padding: 0 !important; border: 0 !important; height: 1px !important; width: 1px !important; overflow: hidden !important; white-space: nowrap !important; } ' +
+                '.sortierbar caption span { font-weight: normal; font-size: .8em; } ' +
+                '.sortbutton svg { margin-left: .2em; height: .7em; } ' +
+                '.sortbutton.sortedasc svg path:last-of-type { fill: currentColor !important; } ' +
+                '.sortbutton.sorteddesc svg path:first-of-type { fill: currentColor!important; } ' +
+                '.sortbutton.sortedasc > span.visually-hidden:first-of-type { display: none; } ' +
+                '.sortbutton.sorteddesc > span.visually-hidden:last-of-type { display: none; } ' +
+                '.sortbutton.unsorted > span.visually-hidden:last-of-type { display: none; } ';
+            sortbuttonStyle.innerText = stylestring;
+            sortbuttonStyle.id = "Stylesheet_tableSort";
+            document.head.appendChild(sortbuttonStyle);
+        }
+        // Kopfzeile vorbereiten
+        for (let i = 0; i < tabletitel.length; i++) initTableHead(i);
+        // Array mit Info, wie Spalte zu sortieren ist, vorbelegen
+        for (let c = 0; c < ncols; c++) sorttype[c] = "n";
+        // Tabelleninhalt in ein Array kopieren
+        for (let r = 0; r < nrows; r++) {
+            arr[r] = [];
+            for (let c = 0, cc; c < ncols; c++) {
+                cc = getData(tr[r].cells[c], c);
+                arr[r][c] = cc;
+                // tr[r].cells[c].innerHTML += "<br>"+cc+"<br>"+sorttype[c]; // zum Debuggen
+            }
+            arr[r][ncols] = tr[r];
+        }
+        // Tabelle die Klasse "is_sortable" geben
+        tab.classList.add("is_sortable");
+        // An caption Hinweis anhängen
+        const caption = tab.caption;
+        if (caption) caption.innerHTML +=
+            "<br><span>Ein Klick auf die Spaltenüberschrift sortiert die Tabelle.</span>";
+    } // tableSort
 
+    initTableHead(sp) {
+        // Kopfzeile vorbereiten
+        let sortbutton = document.createElement("button");
+        sortbutton.type = "button";
+        sortbutton.className = "sortbutton unsorted";
+        sortbutton.addEventListener("click", function (e) {
+            if (e.detail <= 1) tsort(sp);
+        }, false);
+        sortbutton.innerHTML = "<span class='visually-hidden'>" + sort_hint.asc +
+            "</span>" + "<span class='visually-hidden'>" + sort_hint.desc +
+            "</span>" + tabletitel[sp].innerHTML + sortsymbol;
+        tabletitel[sp].innerHTML = "<span class='visually-hidden'>" + tabletitel[
+            sp].innerHTML + "</span>";
+        tabletitel[sp].appendChild(sortbutton);
+        sortbuttons[sp] = sortbutton;
+        tabletitel[sp].abbr = "";
+    }
+
+    getData(ele, col) {
+        // Tabellenfelder auslesen und auf Zahl oder String prüfen
+        let val = ele.textContent;
+        // Tausendertrenner entfernen, und Komma durch Punkt ersetzen
+        const tval = val.replace(/\s/g, "")
+            .replace(",", ".");
+        if (!isNaN(tval) && tval.search(/[0-9]/) != -1) return tval; // Zahl
+        sorttype[col] = "s"; // String
+        return val;
+    }
+
+    vglFkt_s(a, b) {
+        return a[sorted].localeCompare(b[sorted], "de");
+    }
+    vglFkt_n(a, b) {
+        return a[sorted] - b[sorted];
+    }
+    tsort(sp) {
+        if (sp == sorted) { // Tabelle ist schon nach dieser Spalte sortiert, also nur Reihenfolge umdrehen
+            arr.reverse();
+            sortbuttons[sp].classList.toggle("sortedasc");
+            sortbuttons[sp].classList.toggle("sorteddesc");
+            tabletitel[sp].abbr = (tabletitel[sp].abbr == sort_info.asc) ? sort_info
+                .desc : sort_info.asc;
+        } else { // Sortieren 
+            if (sorted > -1) {
+                sortbuttons[sorted].classList.remove("sortedasc");
+                sortbuttons[sorted].classList.remove("sorteddesc");
+                sortbuttons[sorted].classList.add("unsorted");
+                tabletitel[sorted].abbr = "";
+            }
+            sortbuttons[sp].classList.remove("unsorted");
+            sortbuttons[sp].classList.add("sortedasc");
+            sorted = sp;
+            tabletitel[sp].abbr = sort_info.asc;
+            if (sorttype[sp] == "n") arr.sort(vglFkt_n);
+            else arr.sort(vglFkt_s);
+        }
+        for (let r = 0; r < nrows; r++) tbdy.appendChild(arr[r][ncols]); // Sortierte Daten zurückschreiben
+    }
+}
 
 class SudokuTestCaseStorage {
     constructor() {
@@ -3028,33 +3172,30 @@ class SudokuTestCaseStorage {
     }
 
     displayTable(nodeId, tableArray) {
-        let table = document.createElement('table');
-        let tbody = document.createElement('tbody');
-        table.appendChild(tbody);
-
+        let table = document.getElementById(nodeId);
         let k = 0;
         for (let row = 0; row < 9; row++) {
-            let rowElem = document.createElement('tr');
             for (let col = 0; col < 9; col++) {
-                let colElem = document.createElement('td');
+                let cellField = document.createElement('div')
+                cellField.classList.add('cell-field');
                 if (tableArray[k] == '0') {
-                    colElem.innerHTML = " ";
+                    let currentText = document.createTextNode('');
+                    cellField.appendChild(currentText);
                 } else {
-                    colElem.innerHTML = tableArray[k];
+                    let currentText = document.createTextNode(tableArray[k]);
+                    cellField.appendChild(currentText);
                 }
-                //if (row === 2 || row === 5) colElem.style.marginBottom = '10px';
-                if (row === 2 || row === 5) colElem.style.borderBottom = "2px solid white";
-
-                // if (col === 2 || col === 5) colElem.style.marginRight = '10px';
-                if (col === 2 || col === 5) colElem.style.borderRight = "2px solid white";
-
-                rowElem.appendChild(colElem);
+                cellField.style.border = "1px solid white";
+                if (row === 2 || row === 5) {
+                    cellField.style.borderBottom = "4px solid white";
+                }
+                if (col === 2 || col === 5) {
+                    cellField.style.borderRight = "4px solid white";
+                } 
+                table.appendChild(cellField);
                 k++;
             }
-            tbody.appendChild(rowElem);
         }
-        let node = document.getElementById(nodeId);
-        node.appendChild(table);
     }
 
     displayClearTable(nodeId) {
