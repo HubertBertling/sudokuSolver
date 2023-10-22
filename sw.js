2a40b78d46956bbc86a1a820b46034eee8f37891
HubertBertling <h.bertling@web.de>
2023-10-22 21:22:44 +0200

    newWebApp

---
+++ sw.js
@@ -0,0 +1,21 @@
+
+// Change this to your repository name
+var GHPATH = '/sudokuSolver';
+
+// Choose a different app prefix name
+var APP_PREFIX = 'sudo_';
+
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…). 
+// If you don't change the version, the service worker will give your
+// users the old files!
+var VERSION = 'version_00';
+
+// The files to make available for offline use. make sure to add 
+// others to this list
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/css/styles.css`,
+  `${GHPATH}/js/app.js`
+]
