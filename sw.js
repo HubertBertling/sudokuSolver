cbbb9afdb08d0981ff63796b8038f2142d9fe46d
HubertBertling <h.bertling@web.de>
2023-10-23 14:26:30 +0200

    version_04

--- sw.js
+++ sw.js
@@ -9,25 +9,44 @@ var APP_PREFIX = 'sudo_';
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_03';
+var VERSION = 'version_04';
 
 // The files to make available for offline use. make sure to add 
 // others to this list
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
-  `${GHPATH}/css/styles.css`,
-  `${GHPATH}/js/app.js`,
-  `${GHPATH}/sudokuMainApp.js`,
-  `${GHPATH}/fastSolverApp.js`,
-  `${GHPATH}/generatorApp.js`,
-  `${GHPATH}/sudokuCommon.js`,
   `${GHPATH}/widescreen.css`,
   `${GHPATH}/smallscreen.css`,
   `${GHPATH}/verysmallscreen.css`,
   `${GHPATH}/print.css`,
+  `${GHPATH}/dist/winbox.bundle.js`,
+  `${GHPATH}/sudokuMainApp.js`,
+  `${GHPATH}/fastSolverApp.js`,
+  `${GHPATH}/generatorApp.js`,
+  `${GHPATH}/sudokuCommon.js`,
   `${GHPATH}/README.md`,
   `${GHPATH}/help.md`,
-  `${GHPATH}/_config.yml`,
-  `${GHPATH}/dist/winbox.bundle.js`
+  `${GHPATH}/_config.yml`
 ]
+
+/*
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/css/styles.css`,
+  `${GHPATH}/widescreen.css`,
+  `${GHPATH}/smallscreen.css`,
+  `${GHPATH}/verysmallscreen.css`,
+  `${GHPATH}/print.css`,
+  `${GHPATH}/js/app.js`,
+  `${GHPATH}/dist/winbox.bundle.js`,
+  `${GHPATH}/sudokuMainApp.js`,
+  `${GHPATH}/fastSolverApp.js`,
+  `${GHPATH}/generatorApp.js`,
+  `${GHPATH}/sudokuCommon.js`,
+  `${GHPATH}/README.md`,
+  `${GHPATH}/help.md`,
+  `${GHPATH}/_config.yml`
+]
+*/
