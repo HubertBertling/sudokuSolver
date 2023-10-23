d2a01b08040a931588ad9b128a3b867e8ca051e0
HubertBertling <h.bertling@web.de>
2023-10-23 17:39:21 +0200

    Version-06 FB icon

--- sw.js
+++ sw.js
@@ -9,15 +9,13 @@ var APP_PREFIX = 'sudo_';
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_05';
+var VERSION = 'version_06';
 
 // The files to make available for offline use. make sure to add 
 // others to this list
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
-  `${GHPATH}/css/styles.css`,
-  `${GHPATH}/js/app.js`,
   `${GHPATH}/sudokuMainApp.js`,
   `${GHPATH}/fastSolverApp.js`,
   `${GHPATH}/generatorApp.js`,
@@ -26,6 +24,7 @@ var URLS = [
   `${GHPATH}/smallscreen.css`,
   `${GHPATH}/verysmallscreen.css`,
   `${GHPATH}/print.css`,
+  `${GHPATH}/images/icon.png`,
   `${GHPATH}/README.md`,
   `${GHPATH}/help.md`,
   `${GHPATH}/_config.yml`,
