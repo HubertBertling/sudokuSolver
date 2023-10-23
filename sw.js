01a5fe8ccd3185f64b84259fc748a87b54c6f5e1
HubertBertling <h.bertling@web.de>
2023-10-23 11:49:18 +0200

    version 03

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ var APP_PREFIX = 'sudo_';
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_01';
+var VERSION = 'version_03';
 
 // The files to make available for offline use. make sure to add 
 // others to this list
@@ -28,5 +28,6 @@ var URLS = [
   `${GHPATH}/print.css`,
   `${GHPATH}/README.md`,
   `${GHPATH}/help.md`,
-  `${GHPATH}/_config.yml`
+  `${GHPATH}/_config.yml`,
+  `${GHPATH}/dist/winbox.bundle.js`
 ]
