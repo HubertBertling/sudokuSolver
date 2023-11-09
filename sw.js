02528a4d16c152c95f766d13f28a4910b3741995
HubertBertling <h.bertling@web.de>
2023-11-09 11:20:11 +0100

    fb

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ var APP_PREFIX = 'sudo_';
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_31';
+var VERSION = 'version_32';
 
 // The files to make available for offline use. make sure to add 
 // others to this list
