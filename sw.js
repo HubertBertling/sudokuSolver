e9b5c31f653dba542341a9b75ba0e5879f5c9f65
HubertBertling <h.bertling@web.de>
2024-01-10 10:30:55 +0100

    V149 FB SW.JS

--- sw.js
+++ sw.js
@@ -9,16 +9,16 @@ var APP_PREFIX = 'sudo_';
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_148';
+var VERSION = 'version_149';
 
 // The files to make available for offline use. make sure to add 
 // others to this list
 var URLS = [    
   `${GHPATH}/`,
-  `${GHPATH}/widescreen.css`,
-  `${GHPATH}/smallscreenNew.css`,
-  `${GHPATH}/verysmallscreenNew.css`,
-  `${GHPATH}/print.css`,
+  `${GHPATH}/CSS/widescreen.css`,
+  `${GHPATH}/CSS/smallscreenNew.css`,
+  `${GHPATH}/CSS/verysmallscreenNew.css`,
+  `${GHPATH}/CSS/print.css`,
   `${GHPATH}/index.html`,
   `${GHPATH}/help.html`,
   `${GHPATH}/JS/sudokuMainApp.js`,
