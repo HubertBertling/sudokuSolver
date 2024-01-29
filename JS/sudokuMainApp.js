b7c171ffe6694ab174fe13ae8e5ec727ba1e7787
HubertBertling <h.bertling@web.de>
2024-01-29 17:51:51 +0100

    v212

--- JS/sudokuMainApp.js
+++ JS/sudokuMainApp.js
@@ -91,6 +91,29 @@ async function chooseAFile() {
     }
 }
 
+
+self.addEventListener("fetch", (event) => {
+    // Regular requests not related to Web Share Target.
+    if (event.request.method !== "POST") {
+      event.respondWith(fetch(event.request));
+      return;
+    }
+  
+    // Requests related to Web Share Target.
+    event.respondWith(
+      (async () => {
+        const formData = await event.request.formData();
+        const link = formData.get("link") || "";
+        // Instead of the original URL `/save-bookmark/`, redirect
+        // the user to a URL returned by the `saveBookmark()`
+        // function, for example, `/`.
+        const responseUrl = await saveBookmark(link);
+        return Response.redirect(responseUrl, 303);
+      })(),
+    );
+  });
+  
+
 function start() {
 
     sudoApp = new SudokuMainApp();
